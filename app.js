86b318b42477b49c08d185580cef22bbbc694e5c
gmartinez24 <grant.lee.martinez@gmail.com>
2023-12-31 00:36:39 -0600

    created boilerplate layout for ejs files and added navbar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const Movie = require("./models/movie");
 
 mongoose.connect("mongodb://127.0.0.1:27017/shelby-reviews");
@@ -14,6 +15,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
